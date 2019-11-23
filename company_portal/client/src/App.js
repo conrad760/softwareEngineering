b21682d3a3d8a96abc4885c138c09f55f98b5e3e
Conrad Parker <ConradLParker@gmail.com>
2019-11-23 01:32:43 -0600

    Added Cat:octocat: stuff nothing to do with SE

--- company_portal/client/src/App.js
+++ company_portal/client/src/App.js
@@ -1,47 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import EmployeeSearch from './pages/employee-search.page';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            string: 'Conrad'
-        };
-    }
-    render() {
-        return (
-            <div className='App'>
-                <header className='App-header'>
-                    <img src={logo} className='App-logo' alt='logo' />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className='App-link'
-                        href='https://www.conrad-parker.com'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <p>Hey {this.state.string}! Check out my website!</p>
-                    </a>
-                    <div>
-                        <button
-                            onClick={() => this.setState({ string: 'Alex' })}
-                        >
-                            Alex?
-                        </button>
-                        |
-                        <button
-                            onClick={() => this.setState({ string: 'Alex' })}
-                        >
-                            Umayr?
-                        </button>
-                    </div>
-                </header>
-            </div>
-        );
-    }
-}
+const App = () => {
+    return (
+        <Router>
+            <Route path='/cat-employees' component={EmployeeSearch} />
+        </Router>
+    );
+};
 
 export default App;
