39f6df6a9c25e89578f690c1bbbe17b4461bd65d
Conrad Parker <ConradLParker@gmail.com>
2019-11-22 18:46:23 -0600

    :thumbs_up: adding folders and playing with react

--- company_portal/client/src/App.js
+++ company_portal/client/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            string: 'Conrad'
+        };
+    }
+    render() {
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <img src={logo} className='App-logo' alt='logo' />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className='App-link'
+                        href='https://www.conrad-parker.com'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <p>Hey {this.state.string}! Check out my website!</p>
+                    </a>
+                    <div>
+                        <button
+                            onClick={() => this.setState({ string: 'Alex' })}
+                        >
+                            Alex?
+                        </button>
+                        |
+                        <button
+                            onClick={() => this.setState({ string: 'Alex' })}
+                        >
+                            Umayr?
+                        </button>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
