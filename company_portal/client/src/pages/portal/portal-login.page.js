4addb867e12c35897c4d16b4d0bd0b410ab4c0d5
Conrad Parker <ConradLParker@gmail.com>
2019-12-02 13:42:11 -0600

    added login

--- company_portal/client/src/pages/portal/portal-login.page.js
+++ company_portal/client/src/pages/portal/portal-login.page.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './portal-login.style.scss';
+
 const Portal = () => (
-    <div className='portal'>
-        <div className='login-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='username'>username</h1>
-                    <h1 className='password'>password</h1>
-                </div>
-            </div>
+    <div>
+        <div class='pen-title'>
+            <h1>Company Portal</h1>
+        </div>
+        <div class='container'>
+            <div class='card'></div>
+            <div class='card'>
+                <h1 class='title'>Login</h1>
+                <form>
+                    <div class='input-container'>
+                        <input
+                            type='#{type}'
+                            id='#{label}'
+                            required='required'
+                        />
+                        <label for='#{label}'>Username</label>
+                        <div class='bar'></div>
+                    </div>
+                    <div class='input-container'>
+                        <input
+                            type='#{type}'
+                            id='#{label}'
+                            required='required'
+                        />
+                        <label for='#{label}'>Password</label>
+                        <div class='bar'></div>
+                    </div>
+                    <div class='button-container'>
+                        <button>
+                            <span>login</span>
+                        </button>
+                    </div>
+                </form>
+        </div>
         </div>
     </div>
 );
