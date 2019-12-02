be237e0983fab96b5560c481c4f266b105d90b0e
Alex <alexander.rivas@live.com>
2019-12-02 16:25:38 -0600

    auth and Role Schema

--- company_portal/models/Role.js
+++ company_portal/models/Role.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const RoleSchema = new mongoose.Schema({
-
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    adminType: {
+        type: String,
+        required: true
+    },
+    links: {
+        type: [String],
+        default: []
+    }
 });
 
 module.exports = Role = mongoose.model('role', RoleSchema);
