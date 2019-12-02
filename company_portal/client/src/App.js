13fc99462a1dfddb95e6db3592c2d09eb1c075a4
Conrad Parker <ConradLParker@gmail.com>
2019-12-02 02:30:29 -0600

    added an ugly login page with no functionality

--- company_portal/client/src/App.js
+++ company_portal/client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import EmployeeSearch from './pages/employee-search.page';
+import Portal from './pages/portal/portal-login.page'
 
 
 
-
-const App = () => {
+function App() {
 
     return (
+        <div className="">
+            < Portal />
+
         <Router>
             <Route path='/cat-employees' component={EmployeeSearch} />
         </Router>
+        </div>
     );
 };
 
