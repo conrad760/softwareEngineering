9a4cb54d176340e1fe77102a30099e79b022c662
Conrad Parker <ConradLParker@gmail.com>
2019-12-02 16:34:03 -0600

    added links

--- company_portal/client/src/App.js
+++ company_portal/client/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import EmployeeSearch from './pages/employee-search.page';
-import Portal from './pages/portal/portal-login.page'
-
-
+import Portal from './pages/portal/portal-login.page';
 
 function App() {
-
     return (
-        <div className="">
-            < Portal />
-
-        <Router>
-            <Route path='/cat-employees' component={EmployeeSearch} />
-        </Router>
+        <div>
+            <Portal />
+            <Router>
+                <Route path='/global' component={EmployeeSearch} />
+                <Route path='/finance' component={EmployeeSearch} />
+                <Route path='/sales' component={EmployeeSearch} />
+                <Route path='/HR' component={EmployeeSearch} />
+                <Route path='/eng' component={EmployeeSearch} />
+                <Route path='/cat-employees' component={EmployeeSearch} />
+            </Router>
         </div>
     );
-};
+}
 
 export default App;
--- company_portal/server.js
+++ company_portal/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+
 const app = express();
 
 // Connect Database
@@ -11,9 +14,8 @@ app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API is Running nicee'));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
