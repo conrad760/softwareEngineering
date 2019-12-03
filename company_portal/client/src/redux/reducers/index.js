4767113fa0477e2a828e1a6f52ed8858bab7c16b
Conrad Parker <ConradLParker@gmail.com>
2019-12-02 23:49:01 -0600

    added redux stuff and did I mention I changed the icon?

--- company_portal/client/src/redux/reducers/index.js
+++ company_portal/client/src/redux/reducers/index.js
@@ -1,1 +1,8 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import roles from './roles';
+
+const rootReducer = combineReducers({
+    roles: roles
+});
+
+export default rootReducer;
--- company_portal/client/src/redux/reducers/roles.js
+++ company_portal/client/src/redux/reducers/roles.js
@@ -1,15 +1,23 @@
-const roles = ( state = [], actions) => {
-    switch(actions.type) {
+import {
+    GLOBAL_ADMIN,
+    FINANCE_ADMIN,
+    SALES_ADMIN,
+    HR_ADMIN,
+    ENGG_ADMIN
+} from '../../dummbie-links';
+const roles = (state = [], actions) => {
+    switch (actions.type) {
         case 'GLOBAL_ADMIN':
-            return state
+            return GLOBAL_ADMIN;
         case 'FINANCE_ADMIN':
-            return state
+            return FINANCE_ADMIN;
         case 'SALES_ADMIN':
-            return state
+            return SALES_ADMIN;
         case 'HR_ADMIN':
-            return state
+            return HR_ADMIN;
         case 'ENGG_ADMIN':
-            return state
+            return ENGG_ADMIN;
         default:
+            return state;
     }
-}
+};
