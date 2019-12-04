93d5f7186636f3a2cbdc1bfa374d20f3e27043cf
Umayr Kaleem <Umayrkaleem@gmail.com>
2019-12-03 23:01:38 -0600

    i fixed all the icons, please carefully review the changes to make sure i didnt accidently change anything else

--- company_portal/client/src/dummbie-links.js
+++ company_portal/client/src/dummbie-links.js
@@ -2,20 +2,19 @@ export const GLOBAL_ADMIN = [
     {
         link: 'manage',
         linkName: 'Manage User Account',
-        icon: ' fas fa-person-carry fas-2x '
-        
-        ,id:1
+        icon: ' fa fa-user fa-1x ',
+        id:1
     },
     {
         link: 'assignRoles',
         linkName: 'Assign Roles',
-        icon: ' far fa-file far-2x '
+        icon: 'fa fa-user fa-1x  '
         ,id:2
     },
     {
         link: 'help',
         linkName: 'Help Desk',
-        icon: 'fas fa-hands-helping fa-2x '
+        icon: 'fa fa-file fa-1x  '
         ,id:3
     }
 ];
@@ -23,19 +22,19 @@ export const FINANCE_ADMIN = [
     {
         link: 'finance_report',
         linkName: 'Finance Reports',
-        icon: ' far fa-file fa-2x '
+        icon: 'fa fa-file fa-2x '
         ,id:4
     },
     {
         link: 'payable',
         linkName: 'Accounts Payable',
-        icon: ' fa fa-money-check-alt fa-2x '
+        icon: ' fa fa-user fa-1x   '
         ,id:5
     },
     {
         link: 'receivable',
         linkName: 'Accounts Receivables',
-        icon: ' fas fa-person-carry fa-2x '
+        icon: ' fa fa-user fa-1x   '
         ,id:6
     },
     {
@@ -47,13 +46,13 @@ export const FINANCE_ADMIN = [
     {
         link: 'manageUserAccount',
         linkName: 'Manage User Account',
-        icon: ' fa fa-home fa-2x ',
+        icon: ' fa fa-user fa-1x  ',
         id: 25
     },
     {
         link: 'assignRoles',
         linkName: 'Assign Roles',
-        icon: ' fa fa-home fa-2x ',
+        icon: ' fa fa-user fa-1x   ',
         id: 26
     },
     {
@@ -67,31 +66,31 @@ export const SALES_ADMIN = [
     {
         link: 'sales_report',
         linkName: 'Sales Reports',
-        icon: ' far fa-file far-2x '
+        icon: ' fa fa-file fa-2x '
         ,id:8
     },
     {
         link: 'lead',
         linkName: 'Sales Leads',
-        icon: ' fas fa-file-chart-pie fas-2x '
+        icon: 'fa fa-user fa-1x '
         ,id:9
     },
     {
         link: 'demo',
         linkName: 'Sales Demo',
-        icon: ' fas fa-layer-group fas-2x ',
-        id:10
+        icon: ' fa fa-file fa-2x',
+        id:10 
     },
     {
         link: 'manageUserAccount',
         linkName: 'Manage User Account',
-        icon: ' fa fa-home fa-2x ',
+        icon: ' fa fa-user fa-1x ',
         id: 28
     },
     {
         link: 'assignRoles',
         linkName: 'Assign Roles',
-        icon: ' fa fa-home fa-2x ',
+        icon: ' fa fa-file fa-2x ',
         id: 29
     },
     {
@@ -105,49 +104,49 @@ export const HR_ADMIN = [
     {
         link: 'NewHire',
         linkName: 'New Hire',
-        icon: ' far fa-file fa-2x '
+        icon: ' fa fa-user fa-1x  '
         ,id:11
     },
     {
         link: 'onBoarding',
         linkName: 'On-Boarding',
-        icon: ' fas fa-layer-group fa-2x '
-        ,id:12
+        icon: ' fa fa-home fa-2x ',
+        id:12
     },
     {
         link: 'benefit',
         linkName: 'Benefits',
-        icon: 'fas fa-hands-helping fa-2x '
+        icon: 'fa fa-user fa-1x  '
         ,id:13
     },
     {
         link: 'payRoll',
         linkName: 'Payroll',
-        icon: ' fas fa-money-check-alt fa-2x '
+        icon: ' fa fa-user fa-1x '
         ,id:14
     },
     {
         link: 'term',
         linkName: 'Terminations',
-        icon: ' fas fa-clock fa-2x '
+        icon: ' fa fa-user fa-1x '
         ,id:15
     },
     {
         link: 'hrReport',
         linkName: 'HR Reports',
-        icon: ' fas fa-file-chart-pie fa-2x ',
+        icon: 'fa fa-file fa-2x  ',
         id:16
     },
     {
         link: 'manageUserAccount',
         linkName: 'Manage User Account',
-        icon: ' fa fa-home fa-2x ',
+        icon: ' fa fa-user fa-1x ',
         id: 31
     },
     {
         link: 'assignRoles',
         linkName: 'Assign Roles',
-        icon: ' fa fa-home fa-2x ',
+        icon: ' fa fa-user fa-1x  ',
         id: 32
     },
     {
@@ -161,31 +160,31 @@ export const ENG_ADMIN = [
     {
         link: 'appMonitor',
         linkName: 'Application Monitoring',
-        icon: ' fas fa-tv fa-2x ',
+        icon: 'fa fa-user fa-1x  ',
         id:17
     },
     {
         link: 'techSupport',
         linkName: 'Tech Support',
-        icon: 'fas fa-hands-helping fa-2x ',
+        icon: 'fa fa-user fa-1x  ',
         id:18
     },
     {
         link: 'appDevelopment',
         linkName: 'App Development',
-        icon: ' fas fa-layer-group fa-2x ',
+        icon: 'fa fa-user fa-1x  ',
         id:19
     },
     {
         link: 'appAdmin',
         linkName: 'App Admin',
-        icon: ' far fa-file fa-2x '
-        ,id:20
+        icon: 'fa fa-user fa-1x  ',
+        id:20
     },
     {
         link: 'releaseManagement',
         linkName: 'Release Management',
-        icon: ' fas fa-file-chart-pie fa-2x ',
+        icon: 'fa fa-file fa-2x  ',
         id:21
     },
     {
@@ -197,7 +196,7 @@ export const ENG_ADMIN = [
     {
         link: 'assignRoles',
         linkName: 'Assign Roles',
-        icon: ' fa fa-home fa-2x ',
+        icon: ' fa fa-file fa-2x  ',
         id: 23
     },
     {
